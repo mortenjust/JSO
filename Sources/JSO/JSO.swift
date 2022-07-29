f25a263be9ea8e11d658c56ed4f53877b587a8f9
Morten Just <morten.just@genstart.dk>
2022-07-29 10:02:26 +0200

    add tests

--- Sources/JSO/JSO.swift
+++ Sources/JSO/JSO.swift
@@ -1,6 +1,101 @@
-public struct JSO {
-    public private(set) var text = "Hello, World!"
+//
+//  JSO.swift
+//
+//  Created by Morten Just on 7/28/22.
+//
 
-    public init() {
+import Foundation
+
+
+/**
+ Light-weight Axios-like JSON helpers.
+ 
+ Simple call
+ ```
+ let response:ReturnType = try await JSO.post(
+         url:url,
+         body:encodableBody)
+ ```
+ 
+ With bearer
+ ```
+ let response:ReturnType = try await JSO.post(
+    url:url,
+    body:encodableBody,
+    bearerToken:t)
+```
+ 
+ You can also specify the return type explicitly
+ ```
+ let response = try await JSO<ReturnType>.post(...
+ ```
+ 
+ */
+public struct JSO<R:Decodable> {
+    /// Post a JSON payload and get one back. Optional bearer token is added to Authorization header.
+    public static func post<B:Encodable>(url:URL, body:B, bearerToken:String?) async throws -> R   {
+        
+        // prep
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
+        
+        if let bearerToken = bearerToken {
+            request.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
+        }
+        
+        request.httpBody = try JSONEncoder().encode(body)
+        
+        // request
+        
+        let ( data,_ ) = try await JSO.data(for: request)
+        
+//        print(String(data: data, encoding: .utf8))
+        
+        let response = try JSONDecoder().decode(R.self, from: data)
+        return response
+        
+    }
+    
+    // Get a JSON response from a GET request
+    public static func get(url:URL) async throws -> R {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
+        let(data,_) = try await JSO.data(for: request)
+        let response = try JSONDecoder().decode(R.self, from: data)
+        return response
     }
 }
+
+// data task stuff for 10.15 compatibility.
+
+public extension JSO {
+    
+    // HT John Sundell
+    @available(macOS, deprecated: 12.0, message: "Just a reminder to replace this function with Apple's version, released in 12")
+    static func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        var dataTask: URLSessionDataTask?
+        let onCancel = { dataTask?.cancel() }
+
+        return try await withTaskCancellationHandler(
+            handler: {
+                onCancel()
+            },
+            operation: {
+                try await withCheckedThrowingContinuation { continuation in
+                    dataTask = URLSession.shared.dataTask(with: request) { data, response, error in
+                        guard let data = data, let response = response else {
+                            let error = error ?? URLError(.badServerResponse)
+                            return continuation.resume(throwing: error)
+                        }
+                        continuation.resume(returning: (data, response))
+                    }
+                    dataTask?.resume()
+                }
+            }
+        )
+    }
+}
+
--- Tests/JSOTests/JSOTests.swift
+++ Tests/JSOTests/JSOTests.swift
@@ -2,10 +2,26 @@ import XCTest
 @testable import JSO
 
 final class JSOTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(JSO().text, "Hello, World!")
+    
+    func testGET() async throws {
+        struct TestResponse : Codable { let name:String; let city:String }
+        
+        let endpoint = URL(string: "http://echo.jsontest.com/name/morten/city/zurich")!
+        
+        let response:TestResponse = try await JSO.get(url: endpoint)
+        XCTAssertEqual(response.name, "morten")
+        XCTAssertEqual(response.city, "zurich")
+    }
+    
+    func testHeaders() async throws {
+        struct TestHeaders : Decodable { let Accept:String; let Authorization:String }
+        struct Payload : Codable { let name:String }
+        
+        let url = URL(string: "http://headers.jsontest.com/")!
+        let token = "klasdlfjkj3908ljkvxzclkxdzj;"
+        
+        let response:TestHeaders = try await JSO.post(url: url, body: Payload(name: "Morten"), bearerToken: token)
+        XCTAssertEqual(response.Authorization, "Bearer \(token)")
+        XCTAssertEqual(response.Accept, "application/json; charset=utf-8")
     }
 }
